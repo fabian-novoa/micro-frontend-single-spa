94b6d1914b1a9025ba5d20c3ae595187586ba017
Fabian Novoa <fabianleandronovoa@gmail.com>
2024-06-10 17:26:00 -0500

    Compartir dependencias entre los micros

---
+++ clase-04/mf-react/src/components/panel.js
@@ -0,0 +1,9 @@
+const Panel = ()=>{
+    return (
+        <div>
+            <h1>Panel -Component</h1>
+        </div>
+    )
+}
+
+export default Panel;
---
+++ clase-04/mf-react/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports={
+
+    entry: './src/app.js',
+    output:{
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+        libraryTarget: 'system',
+    },
+    module:{
+        rules:[
+            {
+            test: /\.jsx?$/,
+            exclude:/node_modules/,
+            use:{
+                loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    plugins: [],
+    externals: ['single-spa', 'react','react-dom'],
+    devServer:{
+        historyApiFallback: true,
+        port : 9002,
+        headers:{
+            "Access-Control-Allow-Origin":"*",
+        }
+    }
+}
